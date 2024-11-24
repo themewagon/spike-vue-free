45c23d3a2382f28a50f3f1e4cb0f50ec83066c5c
mostofanobi <mntanveer9196@gmail.com>
2024-11-24 11:50:54 +0600

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import MainRoutes from './MainRoutes';
 import AuthRoutes from './AuthRoutes';
 
 export const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/:pathMatch(.*)*',
